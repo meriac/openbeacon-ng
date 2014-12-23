4dc78c9a00e5369050a146b88218f1ae8e2185e1
Milosch Meriac <milosch@meriac.com>
2014-12-23 23:01:37 +0000

    added serial port demo

--- firmware/nRF51/tag-ipv6/src/main.c
+++ firmware/nRF51/tag-ipv6/src/main.c
@@ -23,8 +23,34 @@
 
 */
 #include <openbeacon.h>
+#include <nrf_sdm.h>
 
 void main_entry(void)
 {
-	while(1);
+	uint32_t res;
+	uint8_t enabled;
+	volatile int t;
+
+	/* enabled LED output */
+	nrf_gpio_cfg_output(CONFIG_LED_PIN);
+	nrf_gpio_pin_set(CONFIG_LED_PIN);
+
+	/* enabled input pin */
+	nrf_gpio_cfg_input(CONFIG_SWITCH_PIN, NRF_GPIO_PIN_NOPULL);
+
+	/* initialize UART */
+	uart_init();
+
+	while(1)
+	{
+		res = sd_softdevice_is_enabled(&enabled);
+
+		debug_printf("Hello World (0x%08X, %i)\n", res, enabled);
+		for(t=0; t<100000; t++);
+
+		if(nrf_gpio_pin_read(CONFIG_SWITCH_PIN))
+			nrf_gpio_pin_set(CONFIG_LED_PIN);
+		else
+			nrf_gpio_pin_clear(CONFIG_LED_PIN);
+	}
 }
