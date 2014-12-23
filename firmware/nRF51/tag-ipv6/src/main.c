976f1978b7a3607e7b2d77e1715624c1a2016a82
Milosch Meriac <milosch@meriac.com>
2014-12-23 11:59:00 +0000

    added initial IPv6 support

---
+++ firmware/nRF51/tag-ipv6/src/main.c
@@ -0,0 +1,30 @@
+/***************************************************************
+ *
+ * OpenBeacon.org - nRF51 Main Entry
+ *
+ * Copyright 2013-2014 Milosch Meriac <meriac@openbeacon.de>
+ *
+ ***************************************************************
+
+ This file is part of the OpenBeacon.org active RFID firmware
+
+ OpenBeacon is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OpenBeacon is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include <openbeacon.h>
+
+void main_entry(void)
+{
+	while(1);
+}
