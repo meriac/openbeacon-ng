0dd6e71c0d9ebd7a4ab5a7d6082e82d8897ef7b2
Milosch Meriac <milosch@meriac.com>
2015-03-17 22:53:29 +0000

    create word database

---
+++ firmware/nRF51/tag-mischief/db-update.php
@@ -0,0 +1,48 @@
+#!/usr/bin/env php
+<?php
+
+$word_db = ($argc >=2) ? $argv[1] : '/usr/share/dict/words';
+
+if(!($db = @file($word_db)))
+	exit("Unable to open '$word_db'\n");
+
+function filter_words($filter)
+{
+	global $db;
+
+	$regex = '/^([a-z]*)'.$filter.'$/i';
+
+	$line = sprintf("\nconst char g_prefix_%s[] = \"", $filter);
+
+	foreach($db as $word)
+		if(preg_match($regex, $word, $res))
+		{
+			$s = '\0'.ucfirst($res[1]);
+			if(strlen($s)>10)
+				continue;
+
+			if(strlen($line.$s)>75)
+			{
+				echo "$line\"\n";
+				$line = "\t\"";
+			}
+
+			$line .= $s;
+		}
+	echo "$line\";\n";
+}
+
+echo "#ifndef __DB_H__\n"; 
+echo "#define __DB_H__\n";
+
+filter_words('cal');
+filter_words('matic');
+filter_words('meter');
+filter_words('ferous');
+filter_words('metric');
+filter_words('nated');
+filter_words('stic');
+filter_words('opic');
+filter_words('ected');
+
+echo "\n#endif/*__DB_H__*/\n";
